c15a0cffe9f152706669c56a2a58f49bb593df11
Rincewind34 <rincewind34@wtnet.de>
2020-07-16 15:46:46 +0200

    setup

---
+++ src/main/java/de/rincewind/StringCalculator.java
@@ -0,0 +1,5 @@
+package de.rincewind;
+
+public class StringCalculator {
+	
+}
---
+++ src/test/java/de/rincewind/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package de.rincewind;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+	
+	@Test
+	public void successfulInit() throws Exception {
+		new StringCalculator();
+	}
+	
+}
